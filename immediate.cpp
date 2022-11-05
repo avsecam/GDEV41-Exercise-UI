31d9e7f6d21f2a06555ab53a6854a09153b8bb22
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-11-05 18:15:34 +0800

    Make buttons show changes in immediate mode.

--- immediate.cpp
+++ immediate.cpp
@@ -7,98 +7,96 @@
 #include <vector>
 
 // Struct to encapsulate our UI library
-struct UiLibrary
-{
-    // ID of the currently hot widget
-    int hot = -1;
+struct UiLibrary {
+  // ID of the currently hot widget
+  int hot = -1;
 
-    // ID of the currently active widget
-    int active = -1;
+  // ID of the currently active widget
+  int active = -1;
 
-    // Creates a button with the specified text and bounds
-    // Returns true if this button was clicked in this frame
-    bool Button(int id, const std::string& text, const Rectangle& bounds)
-    {
-        bool result = false;
+  // Creates a button with the specified text and bounds
+  // Returns true if this button was clicked in this frame
+  bool Button(int id, const std::string& text, const Rectangle& bounds) {
+    bool result = false;
+    Color bgColor = GRAY;
+    Color color = BLACK;
 
-        // If this button is the currently active widget, that means
-        // the user is currently interacting with this widget
-        if (id == active)
-        {
-            // If the user released the mouse button while we are active,
-            // register as a click
-            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-            {
-                // Make sure that we are still the hot widget
-                // (User might have dragged mouse to another widget, in which case we will not count as a button click)
-                if (id == hot)
-                {
-                    result = true;
-                }
-
-                // Set ourselves to not be active anymore
-                active = -1;
-            }
+    // If this button is the currently active widget, that means
+    // the user is currently interacting with this widget
+    if (id == active) {
+      color = SKYBLUE;
+      bgColor = DARKGREEN;
+      // If the user released the mouse button while we are active,
+      // register as a click
+      if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+        // Make sure that we are still the hot widget
+        // (User might have dragged mouse to another widget, in which case we
+        // will not count as a button click)
+        if (id == hot) {
+          result = true;
+          bgColor = GREEN;
         }
 
-        // If we are currently the hot widget
-        if (id == hot)
-        {
-            // If the user pressed the left mouse button, that means the user started
-            // interacting with this widget, so we set this widget as active
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                active = id;
-            }
-        }
-
-        // If the mouse cursor is hovering within our boundaries
-        if (CheckCollisionPointRec(GetMousePosition(), bounds))
-        {
-            // Set this widget to be the hot widget
-            hot = id;
-        }
-        // If the mouse cursor is not on top of this widget, and this widget
-        // was previously the hot widget, set the hot widget to -1
-        // We check if this widget was the previously hot widget since there is a possibility that
-        // the hot widget is now a different widget, and we don't want to overwrite that with -1
-        else if (hot == id)
-        {
-            hot = -1;
-        }
-
-        // Draw our button regardless of what happens
-        DrawRectangleRec(bounds, GRAY);
-        DrawText(text.c_str(), bounds.x, bounds.y, 14, BLACK);
-
-        return result;
+        // Set ourselves to not be active anymore
+        active = -1;
+      }
     }
+
+    // If we are currently the hot widget
+    if (id == hot) {
+			if (id != active) {
+				bgColor = GREEN;
+			}
+      // If the user pressed the left mouse button, that means the user started
+      // interacting with this widget, so we set this widget as active
+      if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        active = id;
+      }
+    }
+
+    // If the mouse cursor is hovering within our boundaries
+    if (CheckCollisionPointRec(GetMousePosition(), bounds)) {
+      // Set this widget to be the hot widget
+      hot = id;
+    }
+    // If the mouse cursor is not on top of this widget, and this widget
+    // was previously the hot widget, set the hot widget to -1
+    // We check if this widget was the previously hot widget since there is a
+    // possibility that the hot widget is now a different widget, and we don't
+    // want to overwrite that with -1
+    else if (hot == id) {
+      hot = -1;
+      bgColor = GRAY;
+    }
+
+    // Draw our button regardless of what happens
+    DrawRectangleRec(bounds, bgColor);
+    DrawText(text.c_str(), bounds.x, bounds.y, 14, color);
+
+    return result;
+  }
 };
 
-int main()
-{
-    int windowWidth = 800, windowHeight = 600;
-    InitWindow(windowWidth, windowHeight, "Immediate Mode");
-    SetTargetFPS(60);
+int main() {
+  int windowWidth = 800, windowHeight = 600;
+  InitWindow(windowWidth, windowHeight, "Immediate Mode");
+  SetTargetFPS(60);
 
-    UiLibrary uiLibrary;
+  UiLibrary uiLibrary;
 
-    while (!WindowShouldClose())
-    {
-        ClearBackground(WHITE);
-        BeginDrawing();
-        if (uiLibrary.Button(0, "Hello!", { 10, 10, 80, 40 }))
-        {
-            std::cout << "Hello!" << std::endl;
-        }
-        if (uiLibrary.Button(1, "Hi!", { 100, 10, 80, 40 }))
-        {
-            std::cout << "Hi!" << std::endl;
-        }
-        EndDrawing();
+  while (!WindowShouldClose()) {
+    ClearBackground(WHITE);
+    BeginDrawing();
+    if (uiLibrary.Button(0, "Hello!", {10, 10, 80, 40})) {
+      std::cout << "Hello!" << std::endl;
     }
+    if (uiLibrary.Button(1, "Hi!", {100, 10, 80, 40})) {
+      std::cout << "Hi!" << std::endl;
+    }
+    EndDrawing();
+  }
 
-    CloseWindow();
+  CloseWindow();
 
-    return 0;
+  return 0;
 }
